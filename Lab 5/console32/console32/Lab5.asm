14daacbbcafb809f57231694ba6f635492d0e07f
Johnny-Dean <johndevince@mail.frenostate.edu>
2021-02-28 22:41:18 -0800

    Filled out most of the table except MOD/RM

--- Lab 5/console32/console32/Lab5.asm
+++ Lab 5/console32/console32/Lab5.asm
@@ -14,50 +14,51 @@
 
 .CODE                           ; start of main program code
 main    PROC
-       
-        mov eax, 000001A2h ; initialize before state
-        mov eax, 100       ; exectuing instruction
 
-        mov edx, 0FF754C2Eh ; initialize edx 
-
-        mov edx, dval       ; move -1 into edx
-
-        mov exc, 00003A4Ch ; initialize before state
-        mov dval, -100     ; move -100 into our dval
-
-        mov dval, exc      ; move exc into dval
-
-        mov ebx, 00000000h ; fill ebx
-
-        mov ebx, 128 ; move ebx to 128
-
-        mov exc, 1A2B3C4Dh ; initalize before change
-
-        mov cl, bval ; move bval into our c low
-
-        mov eax, 1A2B3C4Dh ; initalize before change
+        ; Question One
+        mov eax, 000001A2h
+        mov eax, 100    
         
+        ; Question Two
+        mov eax, 1234ABh
+        mov AH, 0
+
+        ; Question Three
+        mov edx, 0FF754C2Eh 
+        mov edx, dval    
+        
+        ; Question Four
+        mov ecx, 00003A4Ch 
+        mov dval, -100     
+        mov dval, ecx     
+
+        ; Question Five
+        mov ebx, 00000000h 
+        mov ebx, 128
+
+        ; Question Six
+        mov ecx, 1A2B3C4Dh 
+        mov CL, bval 
+
+        ; Question Seven 
+        mov eax, 1A2B3C4Dh 
         xchg tval, eax 
 
-        mov dx, 0FF75j
-
+        ; Question Eight
+        mov dx, 0FF75h
         xchg dl, dh
 
+        ; Question Nine
         mov eax, 12BC9A78h
         mov edx, 56DE34F0h
-
         xchg eax, edx
 
-        mov EBX 12345678h
-        mov edx, 56DE34F0
-
-        xchg eax, edx
-
-        mov ebx, 12345678
-        mov edx, 56DE34F0
-
+        ; Question Ten
+        mov ebx, 12345678h
+        mov edx, 9ABCDEF0h
         xchg bx, dx
 
+      
         mov   eax, 0            ; exit with return code 0
         ret
 main    ENDP
